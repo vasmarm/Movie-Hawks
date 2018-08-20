5ec89514d3d09ff0c50391192cbbe3197dbe0c60
vasmarm <sharmavashisth@gmail.com>
2018-08-19 22:17:55 -0500

    Completed dropndown filling with bugs

--- app.py
+++ app.py
@@ -25,16 +25,31 @@ def dataFetch():
                                     database='movie_magic_db')
     c = con.cursor()
 
-    c.execute("""SELECT budget, original_title, popularity, revenue, vote_average, vote_count FROM tmdb_5000_movies order by budget""")
+    # c.execute("""SELECT budget, original_title, popularity, revenue, vote_average, vote_count FROM tmdb_5000_movies order by budget""")
 
+    c.execute("""SELECT * FROM tmdb_5000_movies where budget <> 0 LIMIT 5""")
     for row in c:
         d = {
             'budget': row[0],
-            'original_title': row[1],
-            'popularity': row[2],
-            'revenue': row[3],
-            'voter_average': row[4],
-            'voter_count': row[5]
+            'genres': row[1],
+            'homepage': row[2],
+            'id': row[3],
+            'keywords': row[4],
+            'original_language': row[5],
+            'original_title': row[6],
+            'overview': row[7],
+            'popularity': row[8],
+            'production_companies': row[9],
+            'production_countries': row[10],
+            'release_date': row[11],
+            'revenue': row[12],
+            'runtime': row[13],
+            'spoken_languages': row[14],
+            'status': row[15],
+            'tagline': row[16],
+            'title': row[17],
+            'vote_average': row[18],
+            'vote_count': row[19],
         }
         data.append(d)
     
