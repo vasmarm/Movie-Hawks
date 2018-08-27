d82b6c67ab60930db9b9c488608cefd72a62e94f
vasmarm <sharmavashisth@gmail.com>
2018-08-26 20:19:54 -0500

    Heroku Deployment

--- app.py
+++ app.py
@@ -60,6 +60,5 @@ def dataFetch():
 def renderData():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ = '__main__':
+    app.run(debug = True)
